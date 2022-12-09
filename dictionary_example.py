222b6cf44e4bdb3d3bda7d026c30383470e8af9f
Sachin Acharya <51048248+sachin-acharya-projects@users.noreply.github.com>
2022-12-09 18:03:44 +0545

    Create dictionary_example.py

---
+++ dictionary_example.py
@@ -0,0 +1,11 @@
+key_value_pair = [
+    ("name", "Sachin Acharya"),
+    ("age", 19),
+    ("Address", "Bharatpur-2, Chitwan")
+]
+
+dict_ = {
+    keys: value for keys, value in key_value_pair
+}
+
+print(dict_)
